409c55545663f76009c96c9235fd037dba5e121d
Valdir Torres <valdyrtorres@yahoo.com.br>
2021-01-09 20:49:57 -0300

    Fininsh front-web

--- front-web/src/orders/index.tsx
+++ front-web/src/orders/index.tsx
@@ -1,27 +1,84 @@
 import { useEffect, useState } from 'react';
-import { fetchProducts } from '../api';
+import { toast } from 'react-toastify';
+import { fetchProducts, saveOrder } from '../api';
+import Footer from '../footer';
+import { checkIsSelected } from './helpers';
 import OrderLocation from './orderlocation';
+import OrderSummary from './ordersummary';
 import ProductsList from './ProductsList';
 import StepsHeader from './stepsheader';
+
+import { OrderLocationData, Product } from './types';
 import './styles.css';
-import { OrderLocationdata, Product } from './types';
 
 function Orders() {
     const [products, setProducts] = useState<Product[]>([]);
-    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
+    const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+    const totalPrice = selectedProducts.reduce((sum, item) => {
+        return sum + item.price;
+    }, 0);
 
     useEffect(() => {
         fetchProducts()
             .then(response => setProducts(response.data))
-            .catch(error => console.log(error))
+            .catch(error => {
+                toast.warning(`Erro ao listar produto`);
+            })
     }, []);
 
+    
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = checkIsSelected(selectedProducts, product)
+    
+        if (isAlreadySelected) {
+        const selected = selectedProducts.filter(item => item.id !== product.id);
+        setSelectedProducts(selected);
+        } else {
+        setSelectedProducts(previous => [...previous, product]);
+        }
+    }
+
+    const handleSubmit = () => {
+        const productsIds = selectedProducts.map(({ id }) => ({ id }));
+        const payload = {
+          ...orderLocation!,
+          products: productsIds
+        }
+      
+        // Foi utlizado o toast error para aproveitar a cor vermelha que é default para erro
+        // contudo em nosso caso eh sucesso
+        saveOrder(payload)
+            .then((response) => {
+                toast.error(`Pedido enviado com sucesso! Nº ${response.data.id}`);
+                setSelectedProducts([]);
+        })
+          .catch(() => {
+            toast.warning('Erro ao enviar pedido');
+          })
+    }
+
     return (
-        <div className="orders-container">
-           <StepsHeader />
-           <ProductsList  products={products} />
-           <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
-        </div>
+        <>
+            <div className="orders-container">
+                <StepsHeader />
+                <ProductsList 
+                     products={products} 
+                     onSelectProduct={handleSelectProduct}
+                     selectedProducts={selectedProducts}
+                />
+                <OrderLocation 
+                    onChangeLocation={location => setOrderLocation(location)}
+                />
+                <OrderSummary 
+                    amount={selectedProducts.length} 
+                    totalPrice={totalPrice} 
+                    onSubmit={handleSubmit}
+                />
+            </div>
+            <Footer/>
+        </>
+
     )
 }
 
